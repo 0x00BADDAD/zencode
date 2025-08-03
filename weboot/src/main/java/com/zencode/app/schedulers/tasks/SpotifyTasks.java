c5572d66b342cada12ea97a1e4349f392feb0ae7
0x00BADDAD <tclatharvbhagya@INLTTCL00823.corp.tatacapital.com>
2025-08-03 15:41:14 +0000

    Schedulers, Cache and access tokens working

--- weboot/src/main/java/com/zencode/app/schedulers/tasks/SpotifyTasks.java
+++ weboot/src/main/java/com/zencode/app/schedulers/tasks/SpotifyTasks.java
@@ -9,28 +9,118 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Optional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import org.springframework.web.client.RestClient;
+import org.springframework.http.MediaType;
+
+import java.util.Base64;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zencode.app.web.RespClass;
+import com.github.benmanes.caffeine.cache.Cache;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+import java.util.ArrayList;
+
 
 
 @Component
 public class SpotifyTasks {
+    @Autowired
+    private Cache myCache;
 
-    @Scheduled(fixedRate = 2000)
-    public void fetchCurrentPlayingTrackMetadata() {
-//            RestClient restClient = RestClient.create();
-//            String authString = "Bearer " + accessToken;
-//
-//
-//            RespClass resp = restClient.get()
-//                .uri("https://api.spotify.com/v1/me/player/currently-playing")
-//                .accept(MediaType.APPLICATION_JSON)
-//                .header("Authorization", authString)
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .retrieve()
-//                .body(RespClass.class);
-//            logger.debug("resp is retrived and is: %s".format(resp.toString()));
-//            return resp;
+    private static final Logger logger = LogManager.getLogger(SpotifyTasks.class);
+
+    private static final long THIRTY_MINUTES = 1000 * 60 * 30;
+
+    @Scheduled(fixedRate = THIRTY_MINUTES)
+    public void fetchAccessToken() {
+            RestClient restClient = RestClient.create();
+            Optional<Object> cachedRefreshToken = Optional.ofNullable(myCache.getIfPresent("refreshToken"));
+            if (cachedRefreshToken.isPresent()){
+                String clientId = "9469751d45ca49cea94be50c071a3c65";
+                String clientSecret = "6139b2de2c564d9a977f34c3b27fbda4";
+
+
+
+                String inputString = clientId + ":" + clientSecret;
+
+                // Step 2: Get the UTF-8 encoded bytes of the string.
+                // This is equivalent to the JavaScript TextEncoder().
+                byte[] utf8Bytes = inputString.getBytes(StandardCharsets.UTF_8);
+
+                // Step 3: Encode the UTF-8 bytes to Base64.
+                // This is equivalent to the JavaScript btoa() function.
+                String base64String = Base64.getEncoder().encodeToString(utf8Bytes);
+                String authHeader = "Basic " + base64String;
+                MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+
+                formData.add("grant_type", "refresh_token");
+                formData.add("refresh_token", String.valueOf(cachedRefreshToken.get()));
+
+
+                RespClass resp = restClient.post()
+                    .uri("https://accounts.spotify.com/api/token")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .header("Authorization", authHeader)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(formData)
+                    .retrieve()
+                    .body(RespClass.class);
+                logger.debug("---->scheduler [ref token] worked... resp is: %s".format(resp.toString()));
+                myCache.put("accessToken", resp.getAccessToken());
+
+           }else{
+               logger.debug("Cache doesn't have refresh token!");
+           }
     }
 
+    @Scheduled(fixedRate = 1000)
+    public void fetchCurrSong(){
+            RestClient restClient = RestClient.create();
+            Optional<Object> cachedAccessToken = Optional.ofNullable(myCache.getIfPresent("accessToken"));
+           // TODO: proper error handling on all scheduled tasks 
+            if (cachedAccessToken.isPresent()){
+                String accessToken = String.valueOf(cachedAccessToken.get());
+                String authHeader = "Bearer " + accessToken;
+                JsonNode root = restClient.get()
+                    .uri("https://api.spotify.com/v1/me/player/currently-playing")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .header("Authorization", authHeader)
+                    .retrieve()
+                    .body(JsonNode.class);
+
+                String trackUri = root.path("item").path("href").asText();
+
+                JsonNode root_ = restClient.get()
+                   .uri(trackUri)
+                   .accept(MediaType.APPLICATION_JSON)
+                   .header("Authorization", authHeader)
+                   .retrieve()
+                   .body(JsonNode.class);
+
+                String songName = root_.path("name").asText();
+                List<String> artistsAll = new ArrayList<>();
+
+                JsonNode artists = root_.path("artists");
+                if (artists.isArray()){
+                    for (JsonNode artist: artists){
+                        String artistName = artist.path("name").asText();
+                        artistsAll.add(artistName);
+                    }
+                }
+                logger.debug("Song Name: "+ songName + " Artists: "+ artistsAll.toString());
+            }
+
+    }
     //@Scheduled(cron = "0 */1 * * * *")
     //public void runEveryMinute() {
     //    System.out.println("Task every minute: " + Thread.currentThread().getName());
--- weboot/src/main/java/com/zencode/app/services/CachePreloadService.java
+++ weboot/src/main/java/com/zencode/app/services/CachePreloadService.java
@@ -13,7 +13,17 @@ import org.springframework.web.client.RestClient;
 import java.security.SecureRandom;
 import java.net.URI;
 import java.util.Base64;
-import org.springframework.web.reactive.function.client.ClientResponse;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.web.client.RestClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zencode.app.web.RespClass;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 
 
 @Service
@@ -23,63 +33,91 @@ public class CachePreloadService {
 
     private static final Logger logger = LogManager.getLogger(CachePreloadService.class);
 
-    public RespClass convertResponse(ClientResponse response) {
-        return response.body(RespClass.class);
-    }
 
 
     @PostConstruct // Called after the bean is constructed
     public void loadInitialData() {
+        String clientSecret = "6139b2de2c564d9a977f34c3b27fbda4";
 
         String clientId = "9469751d45ca49cea94be50c071a3c65";
         String redirectUri = "http://127.0.0.1:3000/api/spotify_login_success";
-        SecureRandom sr = new SecureRandom();
-        byte[] bytes = new byte[16];
-        sr.nextBytes(bytes);
-        String csrfToken = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+        //SecureRandom sr = new SecureRandom();
+        //byte[] bytes = new byte[16];
+        //sr.nextBytes(bytes);
+        //String csrfToken = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
 
         RestClient restClient = RestClient.create();
 
         try {
             // Example API endpoint
-            String url = "https://accounts.spotify.com/authorize?client_id=" +clientId+ "&response_type=code" + "&redirect_uri="+redirectUri+"&scope=user-read-playback-state user-read-currently-playing"+"&state="+csrfToken;
+            //String url = "https://accounts.spotify.com/authorize?client_id=" +clientId+ "&response_type=code" + "&redirect_uri="+redirectUri+"&scope=user-read-playback-state user-read-currently-playing"+"&state="+csrfToken;
 
-            RespClass resp = restClient.get()
-                .uri(url)
-                .exchange((request, response) -> {
-                
-                        logger.debug("Initial status code: " + response.getStatusCode());
+           // RespClass resp = restClient.get()
+           //     .uri(url)
+           //     .exchange((request, response) -> {
+           //     
+           //             logger.debug("Initial status code: " + response.getStatusCode());
         
-                        if (response.getStatusCode().is3xxRedirection()) {
-                            URI redirect_Uri = response.getHeaders().getLocation();
-                            logger.debug("Redirecting to: " + redirect_Uri);
+           //             if (response.getStatusCode().is3xxRedirection()) {
+           //                 URI redirect_Uri = response.getHeaders().getLocation();
+           //                 logger.debug("Redirecting to: " + redirect_Uri);
         
-                            // Follow manually
-                            return restClient.get()
-                                .uri(redirect_Uri)
-                                .exchange((req, res) -> {
-                                    logger.debug("Initial status code[second time]: " + res.getStatusCode());
+           //                 // Follow manually
+           //                 return restClient.get()
+           //                     .uri(redirect_Uri)
+           //                     .exchange((req, res) -> {
+           //                         logger.debug("Initial status code[second time]: " + res.getStatusCode());
 
-                                    if(res.getStatusCode().is3xxRedirection()) {
-                                        URI redirect_Uri_ = res.getHeaders().getLocation();
-                                        logger.debug("Again Redirecting to: "+ redirect_Uri_);
+           //                         if(res.getStatusCode().is3xxRedirection()) {
+           //                             URI redirect_Uri_ = res.getHeaders().getLocation();
+           //                             logger.debug("Again Redirecting to: "+ redirect_Uri_);
 
-                                        return restClient.get()
-                                            .uri(redirect_Uri_)
-                                            .retrieve()
-                                            .body(RespClass.class);
-                                    }
-                                    logger.debug("No redirect second time");
-                                    return convertResponse(res);
-                                });
-                        }
-                        return new RespClass();
-                        //return response.body(RespClass.class);
-                });
+           //                             return restClient.get()
+           //                                 .uri(redirect_Uri_)
+           //                                 .retrieve()
+           //                                 .body(RespClass.class);
+           //                         }
+           //                         logger.debug("No redirect second time");
+           //                         return convertResponse(res);
+           //                     });
+           //             }
+           //             return new RespClass();
+           //             //return response.body(RespClass.class);
+           //     });
+            //String accessToken = "BQC_f2oOFLmanJrbCYU1cwFhbRdYJDJa1tTLNRUsqFpjfloyJ-6bW7A6Jw6CQcoPt_hC9NB5J1aO1D9VMMabW4OpTufksrkpajhCBGNj3PjMzD4lBBBFafFU6BN_xs-BPN2__2V0q3T-UCdbXNjfC-Wi1I9eL4XqdFcGZSRxt-nzJYjCM0mgqQKAR-5N1z3eHnIZ3FFhmtSzIaeb723Uc4qw7PWgqyzTlmEAOKaJTUXekJDd0g";
 
-            myCache.put("accessToken", resp.getAccessToken());
-            myCache.put("refreshToken", resp.getRefreshToken());
-            logger.debug("preloaded cache  with: "+ resp.getAccessToken() + " and " + resp.getRefreshToken());
+            String refreshToken = "AQCvmM6MO6ZOK9diYgAKVTMm5aDFR9CYbNIaqHG8BaAb3xPZCjJsrsk5rOwsqg7DEBYsQEv-Q_syPBblSW971XahKWnu0Ch6DIs5-n85umtnRPpT36FN1PQ4rDK43a-KDcw";
+
+                String inputString = clientId + ":" + clientSecret;
+
+                // Step 2: Get the UTF-8 encoded bytes of the string.
+                // This is equivalent to the JavaScript TextEncoder().
+                byte[] utf8Bytes = inputString.getBytes(StandardCharsets.UTF_8);
+
+                // Step 3: Encode the UTF-8 bytes to Base64.
+                // This is equivalent to the JavaScript btoa() function.
+                String base64String = Base64.getEncoder().encodeToString(utf8Bytes);
+                String authHeader = "Basic " + base64String;
+                MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+
+                formData.add("grant_type", "refresh_token");
+                formData.add("refresh_token", refreshToken);
+
+
+                RespClass resp = restClient.post()
+                    .uri("https://accounts.spotify.com/api/token")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .header("Authorization", authHeader)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(formData)
+                    .retrieve()
+                    .body(RespClass.class);
+                logger.debug("---->scheduler [ref token] worked... resp is: %s".format(resp.toString()));
+                myCache.put("accessToken", resp.getAccessToken());
+
+
+            myCache.put("refreshToken", refreshToken);
+            logger.debug("preloaded cache  with: "+ resp.getAccessToken() + " and " + refreshToken);
         } catch (Exception e) {
             logger.debug("Whoops! some error occured while preloading the cache!");
             System.err.println("Failed to preload cache: " + e.getMessage());
